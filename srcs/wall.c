bd101fac4d4b0f6409bf311e67aecfdb8c0030b9
maxencejded <maxence@aermob.com>
2019-08-29 11:28:09 -0700

    [Wall.c] Optimize computing

--- srcs/wall.c
+++ srcs/wall.c
@@ -29,15 +29,16 @@ static double	wall_horizontal(t_game *game, t_vector player, float theta)
 {
 	int			wall;
 	double		x;
+	double		y;
 	t_vector	point;
 
 	wall = 0;
+	x = fabs(BLOCK_SIZE / tan(theta * M_PI / 180));
+	y = BLOCK_SIZE;
 	point_set(&point, player.x, player.y);
-	x = fabs((BLOCK_SIZE / 2) / tan(theta * (M_PI / 180)));
-	point_update(&point, theta, x, BLOCK_SIZE / 2);
-	x = fabs(BLOCK_SIZE / tan(theta * (M_PI / 180))) + 1.0;
+	point_update(&point, theta, x / 2, y / 2);
 	while (SUCCESS == point_is_wall(game, point, &wall) && wall == 0)
-		point_update(&point, theta, x, BLOCK_SIZE + 1.0);
+		point_update(&point, theta, x + 1.0, y + 1.0);
 	if (wall == 0)
 		return (MAXFLOAT);
 	return (sqrt(pow(point.x - player.x, 2) + pow(point.y - player.y, 2)));
@@ -46,16 +47,17 @@ static double	wall_horizontal(t_game *game, t_vector player, float theta)
 static double	wall_vertical(t_game *game, t_vector player, float theta)
 {
 	int			wall;
+	double		x;
 	double		y;
 	t_vector	point;
 
 	wall = 0;
+	x = BLOCK_SIZE;
+	y = fabs(BLOCK_SIZE * tan(theta * M_PI / 180));
 	point_set(&point, player.x, player.y);
-	y = fabs((BLOCK_SIZE / 2) * tan(theta * (M_PI / 180)));
-	point_update(&point, theta, BLOCK_SIZE / 2, y);
-	y = fabs(BLOCK_SIZE * tan(theta * (M_PI / 180))) + 1.0;
+	point_update(&point, theta, x / 2, y / 2);
 	while (SUCCESS == point_is_wall(game, point, &wall) && wall == 0)
-		point_update(&point, theta, BLOCK_SIZE + 1.0, y);
+		point_update(&point, theta, x + 1.0, y + 1.0);
 	if (wall == 0)
 		return (MAXFLOAT);
 	return (sqrt(pow(point.x - player.x, 2) + pow(point.y - player.y, 2)));
